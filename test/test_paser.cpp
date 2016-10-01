f4fafa0e0af0b7650b994bdda95a4dacb634b947
Broglie <broglie141@163.com>
2016-10-01 20:08:54 +0800

    add new test directory

---
+++ test/test_paser.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "http_parser.h"
+
+using namespace std;
+
+int main()
+{
+    string request = "GET /index.html HTTP/1.1\r\nHost: www.zhihu.com\r\nConnection: keep-alive\r\n";
+    parser p(request);
+    http_request result = p.get_parse_result();
+    cout << "Method: " << result.method << endl;
+    cout << "URI: " << result.uri << endl;
+    cout << "Version: " << result.version << endl;
+    cout << "Host: " << result.host << endl;
+    cout << "Connection: " << result.connection << endl;
+    return 0;
+}
